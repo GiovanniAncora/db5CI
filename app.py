58ff475855adc4b8bf29295bae48f2b266d35f7e
Giovanni Ancora <ancora.giovanni.scuola+github@gmail.com>
2024-12-11 12:49:09 +0100

    Creazione pagina dinamica con database alunni

---
+++ app.py
@@ -0,0 +1,18 @@
+import pymysql.cursors
+from flask import Flask, render_template
+
+conn = pymysql.connect(host='172.16.12.54', user='ospite', password='ospite', database='db5CI')
+cursore = conn.cursor()
+query = 'SELECT * FROM alunni'
+cursore.execute(query)
+
+risultato = cursore.fetchall()
+print(risultato)
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', alunni= risultato)
+
+app.run(debug=True)
