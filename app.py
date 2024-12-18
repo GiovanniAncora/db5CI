84a72e3a19a8ddeeb3cbaa5da5964b1aff5a470b
Giovanni Ancora <ancora.giovanni.scuola+github@gmail.com>
2024-12-18 12:58:34 +0100

    Separata la query dell'index per evitare che vengano usati i dati di una tabella per un'altra

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 
 conn = pymysql.connect(host='172.16.12.54', user='ospite', password='ospite', database='db5CI')
 cursore = conn.cursor()
-query = 'SELECT * FROM alunni'
-cursore.execute(query)
 
-risultato = cursore.fetchall()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    query = 'SELECT * FROM alunni'
+    cursore.execute(query)
+    risultato = cursore.fetchall()
     return render_template('index.html', alunni= risultato, campi= cursore.description)
 
 @app.route('/<studente>')
