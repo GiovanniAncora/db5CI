087c6857a45fe213b029a98c785b20b8d3ce6e8c
Giovanni Ancora <ancora.giovanni.scuola+github@gmail.com>
2025-04-02 14:13:31 +0200

    Modificato il python per la pagina di registrazione, di cui ho anche creato l'HTML.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import pymysql.cursors
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, session
 
 conn = pymysql.connect(host='172.16.12.54', user='ospite', password='ospite', database='db5CI')
 cursore = conn.cursor()
 
-
 app = Flask(__name__)
+
+app.secret_key = 'ProvolaCiao'
 
 @app.route('/')
 def index():
@@ -28,5 +29,35 @@ def medie():
     risultatoMedie = cursore.fetchall()
     # print(risultatoMedie)
     return render_template("medie.html", medie=risultatoMedie, campi= cursore.description)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        nuovoUser = request.form['username']
+        nuovaPass = request.form['password']
+        matricola = request.form['matricola']
+
+        queryControllo = "SELECT * FROM utenti WHERE username = %s;"
+        cursore.execute(queryControllo, (nuovoUser,))
+        risultatiControllo = cursore.fetchall()
+
+        if risultatiControllo:
+            print("Utente già registrato.")
+            return redirect(url_for('register'))
+
+        queryControlloMatr = "SELECT matricola FROM alunni WHERE matricola = %s;"
+        cursore.execute(queryControlloMatr, (matricola,))
+        
+        if not cursore.fetchall():
+            print("La matricola non esiste!")
+            return redirect(url_for('register'))
+
+        queryInserimento = """INSERT INTO utenti VALUES (%s, %s, %s)"""
+        cursore.execute(queryInserimento, (nuovoUser, nuovaPass, matricola))
+        conn.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template("register.html")
 
 app.run(debug=True)
